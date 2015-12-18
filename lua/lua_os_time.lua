d704cd6ddda8eb3ed7c6e0e9f5d1774688799733
wyp2013 <smtl2013wyp@gmail.com>
2015-12-18 13:14:11 +0800

    os_time

---
+++ lua/lua_os_time.lua
@@ -0,0 +1,25 @@
+--os.time([])返回据标准时间的差值--
+print(os.time())
+print(os.time({year=1970, month=1, day=1, hour=8, min=0, sec=0}))
+print(os.time({year=1970, month=1, day=1, hour=8, min=1, sec=0}))
+
+--[[
+--返回据现在的时间差值
+--]]
+
+print(os.date("today is %Y-%B-%d %H:%M:%S"))
+print(os.date("today is %x %X"))
+
+print(os.getenv("HOME"))
+
+
+function createDir(dirname)
+    os.execute("mkdir" ..dirname)
+end
+
+
+
+
+
+
+
